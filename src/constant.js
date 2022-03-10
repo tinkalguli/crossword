0aeb9961df1376c7cd601cf97cc7ab82a47cdc1c
tinkalguli <tinkalmunu@gmail.com>
2022-03-11 02:14:30 +0530

    Add audio

--- src/constant.js
+++ src/constant.js
@@ -4,14 +4,37 @@
 //   { word: "lap", positions: [26, 27, 28], hint: "One journey around a running track" }
 // ];
 
+// export const WORDS = [
+//   { word: "blow", positions: [0, 1, 2, 3], hint: "What the wind dose" },
+//   { word: "week", positions: [3, 9, 15, 21], hint: "Seven days" },
+//   { word: "bell", positions: [14, 15, 16, 17], hint: "Church ringer" },
+//   { word: "book", positions: [14, 20, 26, 32], hint: "Something to read" },
+//   { word: "lock", positions: [17, 23, 29, 35], hint: "What a key opens" },
+//   { word: "look", positions: [18, 19, 20, 21], hint: "To focus vision on someone or something" },
+//   { word: "kick", positions: [32, 33, 34, 35], hint: "To strike with the foot" },
+// ];
+
 export const WORDS = [
-  { word: "blow", positions: [0, 1, 2, 3], hint: "What the wind dose" },
-  { word: "week", positions: [3, 9, 15, 21], hint: "Seven days" },
-  { word: "bell", positions: [14, 15, 16, 17], hint: "Church ringer" },
-  { word: "book", positions: [14, 20, 26, 32], hint: "Something to read" },
-  { word: "lock", positions: [17, 23, 29, 35], hint: "What a key opens" },
-  { word: "look", positions: [18, 19, 20, 21], hint: "To focus vision on someone or something" },
-  { word: "kick", positions: [32, 33, 34, 35], hint: "To strike with the foot" },
+  { word: "group", positions: [0, 1, 2, 3, 4], hint: "Number of people considered as a unit" },
+  { word: "out", positions: [6, 7, 8], hint: "Away from home" },
+  { word: "off", positions: [6, 15, 24], hint: "Silence switch" },
+  { word: "throw", positions: [8, 17, 26, 35, 44], hint: "Lose on purpose" },
+  { word: "port", positions: [4, 13, 22, 31], hint: "Harbour" },
+  { word: "often", positions: [2, 11, 20, 29, 38], hint: "Frequently" },
+  { word: "gap", positions: [0, 9, 18], hint: "Space between train and platform" },
+  { word: "pet", positions: [18, 19, 20], hint: "Family pooch, for example" },
+  { word: "sand", positions: [36, 37, 38, 39], hint: "Beach grains" },
+  { word: "sweet", positions: [36, 45, 54, 63, 72], hint: "Containing sugar" },
+  { word: "try", positions: [72, 73, 74], hint: "Give it a whril" },
+  { word: "toy", positions: [56, 65, 74], hint: "Child's amusement device" },
+  { word: "enter", positions: [54, 55, 56, 57, 58], hint: "Knock before you ___" },
+  { word: "aria", positions: [49, 58, 67, 76], hint: "Operatic song for a solo voice" },
+  { word: "alone", positions: [76, 77, 78, 79, 80], hint: "Without company" },
+  { word: "eye", positions: [62, 71, 80], hint: "Organ of sight" },
+  { word: "lie", positions: [60, 61, 62], hint: "Tell it like it isn't" },
+  { word: "hello", positions: [42, 51, 60, 69, 78], hint: "Opposite of goodbye" },
+  { word: "thaw", positions: [41, 42, 43, 44], hint: "Warm weather following a freeze, when snow and ice melt" },
+  { word: "refer", positions: [22, 23, 24, 25, 26], hint: "Make refence to" },
 ];
 
-export const NUMBER_OF_CELL = 36;
+export const NUMBER_OF_CELL = 81;
